b4ca89763d3342af3f08e32847485f96f5d815c6
MUHAMMAD ASIM BANGASH <asimbangash1999@gmail.com>
2023-04-04 11:49:09 +0500

    test

---
+++ src/user/user.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('image',{
+    storage:diskStorage({
+      destination:"./files",
+      filename:(req,file,cb)=>{
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()*1e9);
+        const ext = extname(file.originalname);
+        const filename = `${uniqueSuffix}-${ext}`;
+        cb(null,filename);
+      }
+    })
+  }))
+  create(@UploadedFile() file:Express.Multer.File, @Body() createUserDto: CreateUserDto) {
+    // console.log(file);
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
